8c4f3582d0515b32e8b02cd3ae715851d26104ae
FLzyy <96031953+FLzyy@users.noreply.github.com>
2022-12-15 02:13:58 +0000

    basic functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,65 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 import { createSpinner } from "nanospinner";
-import { writeFile } from "node:fs/promises";
+import { downloadTemplate } from "giget";
 
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+console.clear();
+console.log(chalk.blue.bold("create-discord-bot"));
+
+const questions = [
+    {
+        message: "Where would you like to create the discord bot:",
+        type: "input",
+        name: "directoryPath",
+        default: "./"
+    },
+    {
+        message: "Language to use:",
+        name: "language",
+        type: "list",
+        choices: ["Typescript", "Javascript"],
+        filter(value) {
+            return value.toLowerCase();
+        }
+    },
+    {
+        message: "Enable ESLint:",
+        type: "confirm",
+        name: "eslint"
+    },
+    {
+        message: "Enable Prettier:",
+        type: "confirm",
+        name: "prettier"
+    }
+];
+
+inquirer
+    .prompt(questions)
+    .then(async (answers) => {
+        console.clear();
+
+        const spinner = createSpinner("Setting up your project...");
+        spinner.start();
+        sleep();
+
+        try {
+            await downloadTemplate(
+                `github:flzyy/create-discord-bot/templates/${answers.language}`,
+                {
+                    dir: answers.directoryPath,
+                    force: true
+                }
+            );
+            spinner.success({ text: "Done!" });
+            sleep();
+        } catch (error) {
+            spinner.error({
+                text: `An error has occurred: ${chalk.red(error)}`
+            });
+        }
+    })
+    .catch((error) => {
+        console.error(chalk.red(error));
+    });
