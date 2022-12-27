fa4114ff51e52be438079103d4305b148f89a68d
FLzyy <flzyy72@gmail.com>
2022-12-27 09:03:03 +0800

    formatting stuff

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,7 +3,7 @@ import { dirname, join, resolve } from "path";
 
 const __dirname = (() => {
   const x = dirname(decodeURI(new URL(import.meta.url).pathname));
-  return resolve((process.platform == "win32") ? x.substring(1) : x);
+  return resolve(process.platform == "win32" ? x.substring(1) : x);
 })();
 
 const source = await readFile(join(__dirname, "../../package.json"), "utf-8");
@@ -12,10 +12,10 @@ object.scripts = {};
 object.devDependencies = {};
 await writeFile(
   join(__dirname, "../../dist/package.json"),
-  Buffer.from(JSON.stringify(object, null, 2), "utf-8"),
+  Buffer.from(JSON.stringify(object, null, 2), "utf-8")
 );
 
 await copyFile(
   join(__dirname, "../../README.md"),
-  join(__dirname, "../../dist/README.md"),
+  join(__dirname, "../../dist/README.md")
 );
