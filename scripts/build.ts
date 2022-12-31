d0b40a365ea2e9c74146961fe0eeb2bb731b1c41
FLzyy <flzyy72@gmail.com>
2022-12-31 12:33:22 +0800

    nvm

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,7 @@ import { copyFile, readFile, writeFile } from "fs/promises";
 const source = await readFile("./package.json", "utf-8");
 const object = JSON.parse(source);
 object.scripts = {};
+object.devDependencies = {};
 await writeFile(
   "./dist/package.json",
   Buffer.from(JSON.stringify(object), "utf-8")
