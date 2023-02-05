a26ed7e3c86aaaf7e022a10b15d18195a1d78ff0
FLzyy <flzyy72@gmail.com>
2023-02-05 09:59:08 +0800

    run checking in parallel

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,4 +1,4 @@
-import { execSync } from "child_process";
+import { spawn } from "child_process";
 
 const toCheck = [
   "./templates/javascript/default/package.json",
@@ -7,16 +7,8 @@ const toCheck = [
   "./templates/typescript/pino/package.json",
 ];
 
-console.clear();
-
-for (let i = 0; i < toCheck.length; i++) {
-  console.log(
-    `\x1b[1mCurrently Checking: ${toCheck[i]
-      .replace("./templates/", "")
-      .replace("/package.json", "")}\x1b[0m\n`
-  );
-  execSync(`npx ncu -u --packageFile ${toCheck[i]}`, {
-    stdio: [0, 1, 2],
+toCheck.forEach((value) => {
+  spawn(`npx`, ["ncu", "-u", "--packageFile", value], {
+    stdio: "inherit",
   });
-  console.log("\n");
-}
+});
